f740f281b03dbec2a560db87a143b4b0d9e7baec
pouchedfox <yifang.sun@relevance.ai>
2022-07-15 11:53:16 +1000

    bulk indexing

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from datetime import datetime
-from elasticsearch import Elasticsearch
-import numpy as np
+from elasticsearch import Elasticsearch, helpers
 import csv
+import ast
 
 es = Elasticsearch(
     cloud_id='My_deployment:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQxNzZkZTlkOTlhYTQ0N2U5YTg0YmUxOWQyZjViNDYxMyRiYTQ4NTAwZGQ5ZDg0MDAwODFlNmUxOTg3M2JhNmQ3OQ==',
@@ -19,6 +14,7 @@ def read_csv(filename):
         headers = next(file_data)
         return [dict(zip(headers, i)) for i in file_data]
 
+dim = 768
 
 if __name__ == '__main__':
     mapping = {
@@ -29,7 +25,7 @@ if __name__ == '__main__':
                 },
                 "vector": {
                     "type": "dense_vector",  # formerly "string"
-                    "dims": 5
+                    "dims": dim
                 },
             }
         }
@@ -37,16 +33,19 @@ if __name__ == '__main__':
     es.options(ignore_status=[400, 404]).indices.delete(index='test-index')
     es.options(ignore_status=[400, 404]).indices.create(index="test-index", body=mapping)
 
-    docs = read_csv('sample-movies-123.csv')
-    n = 10
-    for id, movie in enumerate(docs[:n]):
-        doc = {
-            'text': movie['description'],
-            'vector': [id + 1] * 5
+    docs = read_csv('sample-movies-vec.csv')
+    n = len(docs)
+    actions = [
+        {
+            "_index": "test-index",
+            "_id": id,
+            "_source": {
+            "text": movie['description'],
+            'vector': ast.literal_eval(movie['desc_vec'])[:dim]}
         }
-        resp = es.index(index="test-index", id=id, document=doc)
-
-    resp = es.get(index="test-index", id=1)
+        for id, movie in enumerate(docs[:n])
+    ]
+    helpers.bulk(es, actions)
 
     es.indices.refresh(index="test-index")
 
@@ -65,13 +64,13 @@ if __name__ == '__main__':
                 "script": {
                     "source": "1/(1-(cosineSimilarity(params.query_vector, 'vector') + 1.0)/2+0.1)",
                     "params": {
-                        "query_vector": [2, 2, 2, 2, 2]
+                        "query_vector": [1] * dim
                     }
                 }
             }
         },
         "rescore": {
-            "window_size": 10,
+            "window_size": 100,
             "query": {
                 "score_mode": "total",
                 "rescore_query": {
